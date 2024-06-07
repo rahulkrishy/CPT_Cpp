33ae383f10c94a7139fac53da446ed8162ba8a10
Rahul <160257073+rahulkrishy@users.noreply.github.com>
2024-06-07 10:35:16 +0530

    Update _HelloWorld.cpp

--- Cpp_Basic/Basic/_HelloWorld.cpp
+++ Cpp_Basic/Basic/_HelloWorld.cpp
@@ -1,7 +1,23 @@
-#include<iostream>
+// #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
   cout<<"Hello World!"<<endl;
 }
+
+/*
+<bits/stdc++.h> in C++
+It is a header file that includes every standard library.
+
+Advantages of bits/stdc++.h:
+  -Reduces the need to write and remember all necessary header files.
+  -Saves time in contests, especially when rank is time-sensitive.
+  -Simplifies the use of STL functions in GNU C++.
+
+Disadvantages of using bits/stdc++.h:
+  -It's a non-standard GNU C++ header.
+  -It may not work with other compilers, such as MSVC.
+  -It includes many unnecessary files, increasing compilation time.
+*/
