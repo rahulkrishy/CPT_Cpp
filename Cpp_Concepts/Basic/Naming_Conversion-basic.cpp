81effb0b7b514e84899ea14b77ee07d0e7353e35
Rahul <160257073+rahulkrishy@users.noreply.github.com>
2024-09-19 15:53:53 +0530

    Learning Files

---
+++ Cpp_Concepts/Basic/Naming_Conversion-basic.cpp
@@ -0,0 +1,14 @@
+// Naming Conversion
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int file_name;  // Snake Case
+  int FileName;   // Pascal Case (for naming classes)
+  int fileName;   // Camel Case  (for naming variable and constants)
+  int iFileName;  // Hungarian Notation
+
+  return 0;
+}
